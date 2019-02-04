cc853c07763626cefbfbdf59260c9546062782a6
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-04 05:47:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
+  var newArray = [];
+  for (let i=0; i<arrayOfMusicians.length; i++) {
+      newArray.push(arrayOfMusicians[i] + "plays" + arrayOfInstruments[i])
+    }
+  return newArray;
+}
+function johnLennonFacts(facts) {
+  let newArray =[];
+  let i=0;
+ while (facts.length>i) {
+   newArray.push(facts[i] + "!!!");
+   i++:
+ }
+ return newArray;
+}
+function iLoveTheBeatles(number) {
+  let newArray =[];
+  do {
+    newArray.push("I love the Beatles!") 
+  number++;
+}
+  while (number < 15) 
+  return newArray;
+}
